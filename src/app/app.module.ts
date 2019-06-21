056b2b9ea8fa4b506eda808e3977a0240353cd9c
Isuru Hettiarachchi <isuru71@gmail.com>
2019-06-22 01:13:19 +0530

    Add test video player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,12 @@ import { SpinnerComponent } from './shared/spinner.component';
 import { ArchwizardModule } from 'angular-archwizard';
 import { FileDropModule } from 'ngx-file-drop';
 
+// videogular
+import { VgCoreModule } from 'videogular2/core';
+import { VgControlsModule } from 'videogular2/controls';
+import { VgOverlayPlayModule } from 'videogular2/overlay-play';
+import { VgBufferingModule } from 'videogular2/buffering';
+
 import { UploadLectureComponent } from './layouts/upload-lecture/upload-lecture.component';
 import { VideoPlaybackComponent } from './layouts/video-playback/video-playback.component';
 
@@ -53,7 +59,11 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     RouterModule.forRoot(Approutes),
     PerfectScrollbarModule,
     ArchwizardModule,
-    FileDropModule
+    FileDropModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
   ],
   providers: [
     {
