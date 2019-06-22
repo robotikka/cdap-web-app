430fce12ffcd5c528d70b5da444904f6184209be
Isuru Hettiarachchi <isuru71@gmail.com>
2019-06-22 13:30:21 +0530

    Add video player functions

--- src/app/layouts/video-playback/video-playback.component.ts
+++ src/app/layouts/video-playback/video-playback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VgAPI } from 'videogular2/core';
 
 @Component({
   selector: 'app-video-playback',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-playback.component.css']
 })
 export class VideoPlaybackComponent implements OnInit {
+  api: VgAPI;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onPlayerReady(api: VgAPI) {
+    this.api = api;
+
+    this.api.getDefaultMedia().subscriptions.pause.subscribe(() => {
+      console.log('paused');
+    });
+
+    this.api.getDefaultMedia().subscriptions.timeUpdate.subscribe((data) => {
+      console.log(data.srcElement.currentTime);
+      if (Math.floor(data.srcElement.currentTime) === 5) {
+        // console.log('current time fired');
+        this.api.getDefaultMedia().pause();
+      }
+    });
   }
 
 }
