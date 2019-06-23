42985297abfd8fe3f99f977e8143d5eeff9827ae
DamsithKarunaratna <dammakaru@gmail.com>
2019-06-24 00:00:48 +0530

    Complete code editor component

--- src/app/component/code-editor/code-editor.component.ts
+++ src/app/component/code-editor/code-editor.component.ts
@@ -20,87 +20,87 @@ export class CodeEditorComponent implements OnInit {
       seconds: 5
     },
     {
-      content: '&nbsp; public static void main(String[] args) {',
+      content: 'public static void main(String[] args) {',
       timestamp: '0:00:05',
       seconds: 5
     },
     {
-      content: '&nbsp; &nbsp; Collection<Integer> myList = new ArrayList<Integer>();',
+      content: 'Collection<Integer> myList = new ArrayList<Integer>();',
       timestamp: '0:03:40',
       seconds: 220
     },
     {
-      content: '&nbsp; &nbsp; myList.add(10);',
+      content: 'myList.add(10);',
       timestamp: '0:03:15',
       seconds: 195
     },
     {
-      content: '&nbsp; &nbsp; myList.add(20);',
+      content: 'myList.add(20);',
       timestamp: '0:04:05',
       seconds: 245
     },
     {
-      content: '&nbsp; &nbsp; myList.add(30);',
+      content: 'myList.add(30);',
       timestamp: '0:03:15',
       seconds: 195
     },
     {
-      content: '&nbsp; &nbsp; myList.add(50);',
+      content: 'myList.add(50);',
       timestamp: '0:05:20',
       seconds: 320
     },
     {
-      content: '&nbsp; &nbsp; System.out.println("element at index 1 = " + myList.get(1));',
+      content: 'System.out.println("element at index 1 = " + myList.get(1));',
       timestamp: '0:06:55',
       seconds: 415
     },
     {
-      content: '&nbsp; &nbsp; for (Integer var : myList)',
+      content: 'for (Integer var : myList)',
       timestamp: '0:04:20',
       seconds: 260
     },
     {
-      content: '&nbsp; &nbsp; &nbsp; System.out.println(var);',
+      content: 'System.out.println(var);',
       timestamp: '0:05:25',
       seconds: 325
     },
     {
-      content: '&nbsp; &nbsp; ArrayList l1 = new ArrayList();',
+      content: 'ArrayList l1 = new ArrayList();',
       timestamp: '0:07:00',
       seconds: 420
     },
     {
-      content: '&nbsp; &nbsp; ArrayList<Integer> l2 = new ArrayList<Integer>();',
+      content: 'ArrayList<Integer> l2 = new ArrayList<Integer>();',
       timestamp: '0:08:55',
       seconds: 535
     },
     {
-      content: '&nbsp; &nbsp; ArrayList<Integer> l3 = new ArrayList();',
+      content: 'ArrayList<Integer> l3 = new ArrayList();',
       timestamp: '0:08:55',
       seconds: 535
     },
     {
-      content: '&nbsp; &nbsp; Collection l4 = new ArrayList();',
+      content: 'Collection l4 = new ArrayList();',
       timestamp: '0:08:55',
       seconds: 535
     },
     {
-      content: '&nbsp; &nbsp; Collection<Integer> l5 = new ArrayList<Integer>();',
+      content: 'Collection<Integer> l5 = new ArrayList<Integer>();',
       timestamp: '0:08:25',
       seconds: 505
     },
     {
-      content: '&nbsp; &nbsp; Collection<Integer> l6 = new ArrayList();',
+      content: 'Collection<Integer> l6 = new ArrayList();',
       timestamp: '0:08:55',
       seconds: 535
     },
     {
-      content: '&nbsp; &nbsp; List l7 = new ArrayList();',
+      content: 'List l7 = new ArrayList();',
       timestamp: '0:08:55',
       seconds: 535
     },
     {
-      content: '&nbsp; &nbsp; List<Integer> l8 = new ArrayList<Integer>();',
+      content: 'List<Integer> l8 = new ArrayList<Integer>();',
       timestamp: '0:08:55',
       seconds: 535
     },
@@ -119,6 +119,10 @@ export class CodeEditorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(line: Line): void {
+    console.log(line.timestamp);
   }
 
 }
