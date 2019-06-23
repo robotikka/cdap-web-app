c7dab1f2deb054f1130d38de4332b640e926f871
dammakaru <dammakaru@gmail.com>
2019-06-23 11:47:55 +0530

    Add videos to published tab in my uploads

--- src/app/layouts/my-uploads/tabs/published-tab/published-tab.component.ts
+++ src/app/layouts/my-uploads/tabs/published-tab/published-tab.component.ts
@@ -7,7 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublishedTabComponent implements OnInit {
 
-  constructor() { }
+  videos: any[];
+
+  constructor() {
+    this.videos = [
+      {
+        videoTitle: 'test title1',
+        description: 'test description long long long very long',
+        duration: '12:00',
+        thumbnailUrl: 'assets/images/big/img1.jpg'
+      },
+      {
+        videoTitle: 'test title2',
+        description: 'test description long long long very long',
+        duration: '12:00',
+        thumbnailUrl: 'assets/images/big/img1.jpg'
+      },
+      {
+        videoTitle: 'test title3',
+        description: 'test description long long long very long',
+        duration: '12:00',
+        thumbnailUrl: 'assets/images/big/img1.jpg'
+      },
+      {
+        videoTitle: 'test title4',
+        // tslint:disable-next-line:max-line-length
+        description: 'test description long long long very long test description long long long very long test description long long long very long test description long long long very long',
+        duration: '12:00',
+        thumbnailUrl: 'assets/images/big/img1.jpg'
+      }
+    ];
+  }
 
   ngOnInit() {
   }
