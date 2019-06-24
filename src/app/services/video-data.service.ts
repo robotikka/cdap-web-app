27a627b703c269ce43eab691b1b793e1691c3046
DamsithKarunaratna <dammakaru@gmail.com>
2019-06-24 15:54:58 +0530

    Create route resolver to pass data through routes

---
+++ src/app/services/video-data.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+let videos = [
+  {
+    id: '1',
+    videoTitle: 'test title1',
+    description: 'test description long long long very long',
+    duration: '12:00',
+    thumbnailUrl: 'assets/images/big/img1.jpg'
+  },
+  {
+    id: '2',
+    videoTitle: 'test title2',
+    description: 'test description long long long very long',
+    duration: '12:00',
+    thumbnailUrl: 'assets/images/big/img1.jpg'
+  },
+  {
+    id: '3',
+    videoTitle: 'test title3',
+    description: 'test description long long long very long',
+    duration: '12:00',
+    thumbnailUrl: 'assets/images/big/img1.jpg'
+  },
+  {
+    id: '4',
+    videoTitle: 'test title4',
+    // tslint:disable-next-line:max-line-length
+    description: 'test description long',
+    duration: '12:00',
+    thumbnailUrl: 'assets/images/big/img1.jpg'
+  }
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VideoDataService {
+
+  constructor() { }
+
+  getContacts() {
+    return Observable.create(observer => {
+      observer.next(videos);
+    })
+  }
+  
+  getContact(id) {
+    return Observable.create(observer => {
+      setTimeout(() => {
+        observer.next(videos.find((video) => video.id == id))
+        observer.complete();
+      }, 0);
+    });
+  }
+}
+
