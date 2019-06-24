61ad3356bff7e0d5ce3c4b59845e2e08ff578259
Isuru Hettiarachchi <isuru71@gmail.com>
2019-06-24 22:40:49 +0530

    Add question-editor to review video

--- src/app/component/question-editor/question-editor.component.ts
+++ src/app/component/question-editor/question-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
@@ -7,14 +7,12 @@ import { Http } from '@angular/http';
   styleUrls: ['./question-editor.component.css']
 })
 export class QuestionEditorComponent implements OnInit {
-  questions;
+  @Input() questions: any;
+  // questions;
 
-  constructor(private http: Http) { }
+  constructor() { }
 
   ngOnInit() {
-    this.http.get('assets/questions/mock-questions.json').map(data => data.json()).subscribe(data => {
-      this.questions = data;
-    });
   }
 
   logQuestions() {
