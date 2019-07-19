fa181117b49dd4b54d61c15199c6b90cfd1750b0
DamsithKarunaratna <dammakaru@gmail.com>
2019-07-19 08:43:56 +0530

    Add codefiiles

--- src/app/services/video-data.service.ts
+++ src/app/services/video-data.service.ts
@@ -11,110 +11,296 @@ const videos = [
     thumbnailUrl: 'https://cdap-19-087-vtutor-lecturematerials.s3.ap-south-1.amazonaws.com/lec5-thumbnail.jpg',
     code: [
       {
+        title: 'Student.cpp',
         lines: [
           {
-            content: 'import java.util.*;',
-            timestamp: '0:01:55',
-            seconds: 115
+            content: '#include <iostream>',
+            timestamp: '0:04:25',
+            seconds: 265
           },
           {
-            content: 'class Main {',
-            timestamp: '0:00:05',
-            seconds: 5
+            content: 'class Student {',
+            timestamp: '0:31:45',
+            seconds: 1905
           },
           {
-            content: 'public static void main(String[] args) {',
-            timestamp: '0:00:05',
-            seconds: 5
+            content: 'private:',
+            timestamp: '0:07:15',
+            seconds: 435
           },
           {
-            content: 'Collection<Integer> myList = new ArrayList<Integer>();',
-            timestamp: '0:03:40',
-            seconds: 220
+            content: 'int studentNo;',
+            timestamp: '0:31:55',
+            seconds: 1915
           },
           {
-            content: 'myList.add(10);',
-            timestamp: '0:03:15',
-            seconds: 195
+            content: 'int marks1;',
+            timestamp: '0:32:00',
+            seconds: 1920
           },
           {
-            content: 'myList.add(20);',
-            timestamp: '0:04:05',
-            seconds: 245
+            content: 'int marks2;',
+            timestamp: '0:32:00',
+            seconds: 1920
           },
           {
-            content: 'myList.add(30);',
-            timestamp: '0:03:15',
-            seconds: 195
+            content: 'int marks3;',
+            timestamp: '0:32:05',
+            seconds: 1925
           },
           {
-            content: 'myList.add(50);',
-            timestamp: '0:05:20',
-            seconds: 320
-          },
-          {
-            content: 'System.out.println("element at index 1 = " + myList.get(1));',
-            timestamp: '0:06:55',
-            seconds: 415
-          },
-          {
-            content: 'for (Integer var : myList)',
-            timestamp: '0:04:20',
-            seconds: 260
-          },
-          {
-            content: 'System.out.println(var);',
-            timestamp: '0:05:25',
-            seconds: 325
-          },
-          {
-            content: 'ArrayList l1 = new ArrayList();',
+            content: 'public:',
             timestamp: '0:07:00',
             seconds: 420
           },
           {
-            content: 'ArrayList<Integer> l2 = new ArrayList<Integer>();',
-            timestamp: '0:08:55',
-            seconds: 535
+            content: 'void setStudentNo(int no);',
+            timestamp: '0:35:00',
+            seconds: 2100
           },
           {
-            content: 'ArrayList<Integer> l3 = new ArrayList();',
-            timestamp: '0:08:55',
-            seconds: 535
+            content: 'void assignMarks(int mrk1, int mrk2, int mrk3);',
+            timestamp: '0:34:20',
+            seconds: 2060
           },
           {
-            content: 'Collection l4 = new ArrayList();',
-            timestamp: '0:08:55',
-            seconds: 535
+            content: 'float calcAvg();',
+            timestamp: '0:53:45',
+            seconds: 3225
           },
           {
-            content: 'Collection<Integer> l5 = new ArrayList<Integer>();',
-            timestamp: '0:08:25',
-            seconds: 505
+            content: 'void Student::setStudentNo(int no) {',
+            timestamp: '0:36:20',
+            seconds: 2180
           },
           {
-            content: 'Collection<Integer> l6 = new ArrayList();',
-            timestamp: '0:08:55',
-            seconds: 535
+            content: 'studentNo = no;',
+            timestamp: '0:35:55',
+            seconds: 2155
           },
           {
-            content: 'List l7 = new ArrayList();',
-            timestamp: '0:08:55',
-            seconds: 535
+            content: 'void Student::assignMarks(int mrk1, int mrk2, int mrk3) {',
+            timestamp: '0:37:10',
+            seconds: 2230
           },
           {
-            content: 'List<Integer> l8 = new ArrayList<Integer>();',
-            timestamp: '0:08:55',
-            seconds: 535
+            content: 'marks1 = mrk1;',
+            timestamp: '0:58:25',
+            seconds: 3505
           },
           {
-            content: 'List<Integer> l9 = new ArrayList();',
-            timestamp: '0:08:55',
-            seconds: 535
+            content: 'marks2 = mrk2;',
+            timestamp: '0:58:25',
+            seconds: 3505
+          },
+          {
+            content: 'marks3 = mrk3;',
+            timestamp: '0:58:25',
+            seconds: 3505
+          },
+          {
+            content: 'float calcAvg() {',
+            timestamp: '0:53:45',
+            seconds: 3225
+          },
+          {
+            content: 'float avg = (marks1+marks2+marks3)/3.0;',
+            timestamp: '0:58:25',
+            seconds: 3505
+          },
+          {
+            content: 'return avg;',
+            timestamp: '0:58:50',
+            seconds: 3530
+          },
+          {
+            content: 'using namespace std;',
+            timestamp: '0:53:00',
+            seconds: 3180
+          },
+          {
+            content: 'int main() {',
+            timestamp: '0:35:40',
+            seconds: 2140
+          },
+          {
+            content: 'Student std1, std2;',
+            timestamp: '0:58:50',
+            seconds: 3530
+          },
+          {
+            content: 'std1.setStudentNo(100);',
+            timestamp: '0:59:00',
+            seconds: 3540
+          },
+          {
+            content: 'std1.assignMarks(50, 60, 70);',
+            timestamp: '0:56:25',
+            seconds: 3385
+          },
+          {
+            content: 'cout << "Average of student 1 = " << std1.calcAvg()',
+            timestamp: '0:57:45',
+            seconds: 3465
+          },
+          {
+            content: '<< endl;',
+            timestamp: '0:57:35',
+            seconds: 3455
+          },
+          {
+            content: 'return 0;',
+            timestamp: '0:53:00',
+            seconds: 3180
           }
-        ],
-        title: 'Main.java'
-      }],
+        ]
+      },
+      {
+        title: 'Rectangle.cpp',
+        lines: [
+          {
+            content: '#include <iostream>',
+            timestamp: '0:04:25',
+            seconds: 265
+          },
+          {
+            content: 'class Rectangle {',
+            timestamp: '0:04:50',
+            seconds: 290
+          },
+          {
+            content: 'private:',
+            timestamp: '0:07:15',
+            seconds: 435
+          },
+          {
+            content: 'int length;',
+            timestamp: '0:07:15',
+            seconds: 435
+          },
+          {
+            content: 'int width;',
+            timestamp: '0:07:15',
+            seconds: 435
+          },
+          {
+            content: 'public:',
+            timestamp: '0:07:00',
+            seconds: 420
+          },
+          {
+            content: 'void setWidth(int no);',
+            timestamp: '0:09:35',
+            seconds: 575
+          },
+          {
+            content: 'void setLength(int no);',
+            timestamp: '0:09:35',
+            seconds: 575
+          },
+          {
+            content: 'int calcArea();',
+            timestamp: '0:11:00',
+            seconds: 660
+          },
+          {
+            content: 'void Rectangle::setWidth(int no) {',
+            timestamp: '0:16:10',
+            seconds: 970
+          },
+          {
+            content: 'width = no;',
+            timestamp: '0:16:10',
+            seconds: 970
+          },
+          {
+            content: 'void Rectangle::setLength(int no) {',
+            timestamp: '0:18:15',
+            seconds: 1095
+          },
+          {
+            content: 'length = no;',
+            timestamp: '0:16:10',
+            seconds: 970
+          },
+          {
+            content: 'int Rectangle::calcArea() {',
+            timestamp: '0:18:15',
+            seconds: 1095
+          },
+          {
+            content: 'int area = length * width;',
+            timestamp: '0:18:15',
+            seconds: 1095
+          },
+          {
+            content: 'return area;',
+            timestamp: '0:18:15',
+            seconds: 1095
+          },
+          {
+            content: 'using namespace std;',
+            timestamp: '0:53:00',
+            seconds: 3180
+          },
+          {
+            content: 'int main() {',
+            timestamp: '0:35:40',
+            seconds: 2140
+          },
+          {
+            content: 'int no1, no2;',
+            timestamp: '0:41:20',
+            seconds: 2480
+          },
+          {
+            content: 'Rectangle rect1, rect2;',
+            timestamp: '0:41:50',
+            seconds: 2510
+          },
+          {
+            content: 'rect1.setLength(10);',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: 'rect1.setWidth(5);',
+            timestamp: '0:48:35',
+            seconds: 2915
+          },
+          {
+            content: 'rect2.setLength(20);',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: 'rect2.setWidth(10);',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: 'cout << "Area of rectangle 1 = " <<',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: 'rect1.calcArea() << endl;',
+            timestamp: '0:43:55',
+            seconds: 2635
+          },
+          {
+            content: 'cout << "Area of rectangle 2 = " <<',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: 'rect2.calcArea() << endl;',
+            timestamp: '0:44:25',
+            seconds: 2665
+          }
+        ]
+      }
+   ],
     topics: [
       {
         title: 'Public and Private attributes',
@@ -246,6 +432,263 @@ const videos = [
     description: '2018 Object Oriented Concepts Lecture 06',
     duration: '01:01:11',
     thumbnailUrl: 'https://cdap-19-087-vtutor-lecturematerials.s3.ap-south-1.amazonaws.com/lec6-thumbnail.jpg',
+    code: [
+      {
+        title: 'StopWatch.cpp',
+        lines: [
+          {
+            content: '#include <iostream>',
+            timestamp: '0:20:05',
+            seconds: 1205
+          },
+          {
+            content: 'int StopWatch::getMinute() {',
+            timestamp: '0:24:30',
+            seconds: 1470
+          },
+          {
+            content: 'return minute;',
+            timestamp: '0:21:50',
+            seconds: 1310
+          },
+          {
+            content: 'void StopWatch::setMinute(int min) {',
+            timestamp: '0:24:30',
+            seconds: 1470
+          },
+          {
+            content: 'minute = min;',
+            timestamp: '0:24:30',
+            seconds: 1470
+          },
+          {
+            content: 'void StopWatch::setMinute(int min) {',
+            timestamp: '0:24:30',
+            seconds: 1470
+          },
+          {
+            content: 'if (min >= 0 && min <= 59)',
+            timestamp: '0:24:30',
+            seconds: 1470
+          },
+          {
+            content: 'minute = min;',
+            timestamp: '0:24:30',
+            seconds: 1470
+          },
+          {
+            content: 'else',
+            timestamp: '0:10:15',
+            seconds: 615
+          },
+          {
+            content: 'minute = 0;',
+            timestamp: '0:24:30',
+            seconds: 1470
+          },
+          {
+            content: 'int main() {',
+            timestamp: '0:04:20',
+            seconds: 260
+          },
+          {
+            content: 'std::cout << "Hello world\\n" << endl;',
+            timestamp: '0:45:05',
+            seconds: 2705
+          }
+        ]
+      },
+      {
+        title: 'Time.cpp',
+        lines: [
+          {
+            content: '#include <iostream>',
+            timestamp: '0:20:05',
+            seconds: 1205
+          },
+          {
+            content: 'class Time {',
+            timestamp: '0:43:55',
+            seconds: 2635
+          },
+          {
+            content: 'private:',
+            timestamp: '0:03:35',
+            seconds: 215
+          },
+          {
+            content: 'int hour;',
+            timestamp: '0:43:55',
+            seconds: 2635
+          },
+          {
+            content: 'int minute;',
+            timestamp: '0:24:25',
+            seconds: 1465
+          },
+          {
+            content: 'int second;',
+            timestamp: '0:24:25',
+            seconds: 1465
+          },
+          {
+            content: 'public:',
+            timestamp: '0:01:40',
+            seconds: 100
+          },
+          {
+            content: 'void setTime(int h, int m, int s);',
+            timestamp: '0:43:55',
+            seconds: 2635
+          },
+          {
+            content: 'void printTimeUniversal();',
+            timestamp: '0:43:55',
+            seconds: 2635
+          },
+          {
+            content: 'void printTimestandard();',
+            timestamp: '0:43:55',
+            seconds: 2635
+          },
+          {
+            content: 'void Time::setTime(int h, int m, int s) {',
+            timestamp: '0:41:05',
+            seconds: 2465
+          },
+          {
+            content: 'hour = h;',
+            timestamp: '0:49:25',
+            seconds: 2965
+          },
+          {
+            content: 'minute = m;',
+            timestamp: '0:49:25',
+            seconds: 2965
+          },
+          {
+            content: 'second = s;',
+            timestamp: '0:49:25',
+            seconds: 2965
+          },
+          {
+            content: 'void Time::printTimeUniversal() {',
+            timestamp: '0:45:05',
+            seconds: 2705
+          },
+          {
+            content: 'cout << hour << ":" << minute << ":"',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: '<< second << endl;',
+            timestamp: '0:44:40',
+            seconds: 2680
+          },
+          {
+            content: 'void Time::printTimestandard() {',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: 'if (hour > 12) {',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: 'cout << (hour-12) << ":" << minute << ":"',
+            timestamp: '0:46:20',
+            seconds: 2780
+          },
+          {
+            content: '<< second " PM" << endl;',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: 'else {',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: 'cout << hour << ":" << minute << ":"',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: '<< second << " AM" << endl;',
+            timestamp: '0:48:20',
+            seconds: 2900
+          },
+          {
+            content: 'int main() {',
+            timestamp: '0:04:20',
+            seconds: 260
+          },
+          {
+            content: 'Time myTime;',
+            timestamp: '0:56:25',
+            seconds: 3385
+          },
+          {
+            content: 'int hrs, min, sec;',
+            timestamp: '0:55:50',
+            seconds: 3350
+          },
+          {
+            content: 'cout << "Enter Hour ";',
+            timestamp: '0:56:25',
+            seconds: 3385
+          },
+          {
+            content: 'cin >> hrs;',
+            timestamp: '0:56:25',
+            seconds: 3385
+          },
+          {
+            content: 'cout << "Enter Minute ";',
+            timestamp: '0:56:25',
+            seconds: 3385
+          },
+          {
+            content: 'cin >> min;',
+            timestamp: '0:56:25',
+            seconds: 3385
+          },
+          {
+            content: 'cout << "Enter Second ";',
+            timestamp: '0:56:25',
+            seconds: 3385
+          },
+          {
+            content: 'cin >> sec;',
+            timestamp: '0:56:55',
+            seconds: 3415
+          },
+          {
+            content: 'myTime.setTime(hrs, min, sec);',
+            timestamp: '0:56:55',
+            seconds: 3415
+          },
+          {
+            content: 'myTime.printTimeUniversal();',
+            timestamp: '0:57:35',
+            seconds: 3455
+          },
+          {
+            content: 'myTime.printTimestandard();',
+            timestamp: '0:57:35',
+            seconds: 3455
+          },
+          {
+            content: 'return 0;',
+            timestamp: '1:00:20',
+            seconds: 3620
+          }
+        ]
+      }
+    ],
     topics: [
       {
         title: 'Classes and Objects',
@@ -314,42 +757,19 @@ const reviewVideos = [
     description: 'test description long long long very long',
     duration: '12:00',
     thumbnailUrl: 'assets/images/big/img1.jpg'
-  },
-  {
-    videoTitle: 'test title2',
-    description: 'test description long long long very long',
-    duration: '12:00',
-    thumbnailUrl: 'assets/images/big/img1.jpg'
-  },
-  {
-    videoTitle: 'test title3',
-    description: 'test description long long long very long',
-    duration: '12:00',
-    thumbnailUrl: 'assets/images/big/img1.jpg'
-  },
-  {
-    videoTitle: 'test title4',
-    // tslint:disable-next-line:max-line-length
-    description: 'test description long long long very long test description long long long very long test description long long long very long test description long long long very long',
-    duration: '12:00',
-    thumbnailUrl: 'assets/images/big/img1.jpg'
   }
 ];
 const processingVideos = [
   {
-    videoTitle: 'Video 001',
+    videoTitle: 'OOC Lecture 9',
     options: ['match slides', 'match code']
   },
   {
-    videoTitle: 'Video 002',
+    videoTitle: 'OOC Lecture 10',
     options: ['match slides']
   },
   {
-    videoTitle: 'Video 003',
-    options: ['match slides', 'match code']
-  },
-  {
-    videoTitle: 'Video 004',
+    videoTitle: 'OOC Lecture 11',
     options: ['denoise', 'match code']
   }
 ];
