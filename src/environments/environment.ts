f43fcece03cccd3415e2814723a7361f19874e2d
dammakaru <dammakaru@gmail.com>
2019-07-26 17:22:27 +0530

    Change EC2 URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+const ec2URL = 'http://ec2-35-154-98-108.ap-south-1.compute.amazonaws.com:3000/';
 
 export const environment = {
   production: false,
-  upload_url: 'http://ec2-52-66-30-76.ap-south-1.compute.amazonaws.com:3000/v1/videos/upload',
+  upload_url: ec2URL + 'v1/videos/upload',
   // upload_url: 'https://v-tutor-backend.herokuapp.com/v1/videos/upload'
-  // upload_url: 'http://localhost:3000/v1/videos/upload'
-  authnetication_url: 'http://ec2-52-66-30-76.ap-south-1.compute.amazonaws.com:3000/v1/user/authenticate',
-  signup_url: 'http://ec2-52-66-30-76.ap-south-1.compute.amazonaws.com:3000/v1/user/signup'
+  authnetication_url: ec2URL + 'v1/user/authenticate',
+  signup_url: ec2URL + 'v1/user/signup'
 };
