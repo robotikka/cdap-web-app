efaab7ad2f6a299ded5f105287d547afe5739cb7
Stefinie fernando <minolispencer@gmail.com>
2019-08-01 15:22:54 +0530

    Slide matching UI and topics modifications

--- src/app/component/slide-match/slide-match.component.ts
+++ src/app/component/slide-match/slide-match.component.ts
@@ -13,20 +13,20 @@ export class SlideMatchComponent implements OnInit {
   public noOfSlides = 6;
 
   product: any[] = [
-    {id: 157, no: 1, url: '../../../assets/slides/frame0001.jpg'},
-    {id: 160, no: 2, url: '../../../assets/slides/frame0036.jpg'},
-    {id: 166, no: 3, url: '../../../assets/slides/frame0048.jpg'},
-    {id: 1048, no: 4, url: '../../../assets/slides/frame0087.jpg'},
-    {id: 1087, no: 5, url: '../../../assets/slides/frame0117.jpg'},
-    {id: 1089, no: 6, url: '../../../assets/slides/frame0483.jpg'},
-    {id: 1172, no: 1, url: '../../../assets/slides/frame0001.jpg'},
-    {id: 2299, no: 2, url: '../../../assets/slides/frame0036.jpg'},
-    {id: 2346, no: 3, url: '../../../assets/slides/frame0048.jpg'},
-    {id: 2368, no: 4, url: '../../../assets/slides/frame0087.jpg'},
-    {id: 3177, no: 5, url: '../../../assets/slides/frame0117.jpg'},
-    {id: 3191, no: 6, url: '../../../assets/slides/frame0483.jpg'},
-    {id: 3195, no: 5, url: '../../../assets/slides/frame0117.jpg'},
-    {id: 3549, no: 6, url: '../../../assets/slides/frame0483.jpg'}];
+    {id: 157, no: 1, url: '../../../assets/slides/slide1.png'},
+    {id: 160, no: 2, url: '../../../assets/slides/slide2.png'},
+    {id: 166, no: 3, url: '../../../assets/slides/slide3.png'},
+    {id: 1048, no: 4, url: '../../../assets/slides/slide4.png'},
+    {id: 1087, no: 5, url: '../../../assets/slides/slide5.png'},
+    {id: 1089, no: 6, url: '../../../assets/slides/slide6.png'},
+    {id: 1172, no: 7, url: '../../../assets/slides/slide7.png'},
+    {id: 2299, no: 8, url: '../../../assets/slides/slide8.png'},
+    {id: 2346, no: 9, url: '../../../assets/slides/slide9.png'},
+    {id: 2368, no: 10, url: '../../../assets/slides/slide10.png'},
+    {id: 3177, no: 11, url: '../../../assets/slides/slide11.png'},
+    {id: 3191, no: 12, url: '../../../assets/slides/slide12.png'},
+    {id: 3195, no: 13, url: '../../../assets/slides/slide13.png'},
+    {id: 3549, no: 14, url: '../../../assets/slides/slide14.png'}];
 
   images: Array<any> = [];
 
@@ -42,9 +42,10 @@ export class SlideMatchComponent implements OnInit {
   ngOnInit() {
   }
 
-  getTime(time: number): void {
+  getTime(time: number, number): void {
     // alert(time);
     this.vpb.seekVideo(time);
+    this.currentSlide = number
   }
 
   setImage(url: any): void {
